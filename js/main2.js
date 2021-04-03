e65173644cdb594a787b8352845ff7bf730fca6d
7442charles <55584337+7442charles@users.noreply.github.com>
2021-04-03 19:41:47 +0300

    new content&Errors fixed

--- js/main2.js
+++ js/main2.js
@@ -65,3 +65,51 @@ function menuplanningPageJumper(){
     window.location.assign("menuplanning.html");
   }*/
 }
+
+function allUnits(){
+  /*var h3 = document.createElement('h3');
+  h3 = document.createTextNode(<a href="index.html">Home</a>, <br></br>, <a href = "financepg1.html">Finance</a>);
+  h3.setAttribute('id', 'allUnits');
+  h3.appendChild(h3);
+  document.getElementById('flexContainer200').appendChild(h3);*/
+  var anchor = document.createElement('a');
+  var linkelement = document.createTextNode('Finance', + '/n');
+  anchor.setAttribute('id', 'anchorLink');
+  anchor.appendChild(linkelement);
+  document.getElementById('flexContainer200').appendChild(anchor); 
+}
+ 
+
+ /*var h1 = document.createElement('h1');
+  var answerResult = document.createTextNode('Finance');
+  h1.setAttribute('id', 'daysEquiv');
+  h1.appendChild(answerResult);
+  document.getElementById('flexContainer200').appendChild(h1);*/
+
+  /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+if (!e.target.matches('.dropbtn')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  for (var d = 0; d < dropdowns.length; d++) {
+    var openDropdown = dropdowns[d];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
+
+function darkTheme(){
+  //document.getElementsByClassName('darkTheme');
+  var body = document.getElementsByClassName('financeheader');
+  //body.style.backgroundcolor = "black";
+  body.style.color = "black";
+  console.log('darktheme clicked');
+} 
